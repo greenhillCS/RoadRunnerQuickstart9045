203f74e95af1e67386278e26529e125b373af9b9
ZhaoT28 <zhaot28@greenhill.org>
2024-09-18 09:59:13 -0500

    Tiger's Drone Test Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testFiles/TigerTestTeleop.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.testFiles;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@TeleOp(name = "Drone Test Tiger")
+public class TigerTestTeleop extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor DroneMotor = hardwareMap.get(DcMotor.class, "DroneMotor");
+        Servo DroneServo = hardwareMap.get(Servo.class, "DroneServo");
+        DroneMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        while(opModeIsActive()){
+            DroneMotor.setPower(gamepad1.right_trigger-gamepad1.left_trigger);
+            if (gamepad1.dpad_up){
+                DroneServo.setPosition(0);
+            }
+            if (gamepad1.dpad_down){
+                DroneServo.setPosition(1);
+            }
+        }
+    }
+}
