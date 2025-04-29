2f928141156855ff4efb30dbffd20babe94e0cad
ZhaoT28 <zhaot28@greenhill.org>
2025-04-29 08:36:40 -0500

    Tiger - distance spinner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ABCD_TeamTraining/Team_A/distanceSpinner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ABCD_TeamTraining/Team_A/distanceSpinner.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
@@ -15,11 +16,15 @@ public class distanceSpinner extends LinearOpMode {
     private double scale = 0.01;
     DistanceSensor distanceSensor1;
     DcMotor motor;
+    DcMotor motor2;
+    Servo servo;
 
     @Override
     public void runOpMode() throws InterruptedException {
         distanceSensor1 = hardwareMap.get(DistanceSensor.class, "ds");
         motor = hardwareMap.get(DcMotor.class, "motor");
+        motor2 = hardwareMap.get(DcMotor.class, "motor2");
+        servo = hardwareMap.get(Servo.class, "servo");
         motor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
@@ -29,6 +34,13 @@ public class distanceSpinner extends LinearOpMode {
             telemetry.addData("distance", distance);
             telemetry.addData("power", power);
             motor.setPower(power);
+            motor2.setPower(power);
+            if (gamepad1.a) {
+                servo.setPosition(1);
+            }
+            if (gamepad1.b){
+                servo.setPosition(0);
+            }
         }
     }
 }
